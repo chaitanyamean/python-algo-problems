2c2990fafb8a59a47646d08d6996c554cca887cf
chaitanyamean <chaitanyamean@gmail.com>
2021-03-18 11:06:28 +0530

    point is inside or outside cirlce problem

---
+++ simpleProblems/distanceBtwTwoPoints.py
@@ -0,0 +1,21 @@
+
+##  find if the point is inside circle or not
+import math
+
+(Cx, Cy, r) = input().split()
+
+(Px, Py) = input().split()
+
+
+Cx = float(Cx)
+Cy = float(Cy)
+r = float(r)
+Px = float(Px)
+Py = float(Py)
+
+d = math.sqrt((Cx - Px) ** 2 + (Cy - Py) ** 2)
+
+if d < r:
+    print('Point is inside Circle')
+else:
+    print('Point is outside circle')
